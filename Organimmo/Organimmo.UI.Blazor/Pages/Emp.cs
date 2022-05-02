12b45f1bbc2f0fb158162b47aab8f33256cc45cd
TinaBianca <bianca.capatina@student.vives.be>
2022-05-02 12:36:14 +0200

    Test table

---
+++ Organimmo/Organimmo.UI.Blazor/Pages/Emp.cs
@@ -0,0 +1,10 @@
+﻿namespace Organimmo.UI.Blazor.Pages
+{
+    public class Emp
+    {
+       
+            public int empid { get; set; }
+            public string empname { get; set; }
+            public string empcountry { get; set; }
+    }
+}
