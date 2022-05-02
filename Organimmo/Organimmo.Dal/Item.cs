4e71cbd7eeecddfa7462fa5b6c83f0b3213f63a4
Christophe Hemeryck <r0345896@student.vives.be>
2022-05-02 11:18:01 +0200

    Add Models

---
+++ Organimmo/Organimmo.Dal/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Organimmo.Dal
+{
+    public class Item
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string BaseText { get; set; }
+        public string CurrentText { get; set; }
+    }
+}
