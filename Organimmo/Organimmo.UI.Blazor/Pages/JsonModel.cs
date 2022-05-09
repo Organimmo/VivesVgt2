1cdf6c884166d45021f8ca218b761a571826b526
TinaBianca <bianca.capatina@student.vives.be>
2022-05-09 10:49:31 +0200

    Add MudTable to the project

---
+++ Organimmo/Organimmo.UI.Blazor/Pages/JsonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Organimmo.UI.Blazor.Pages
+{
+    public class JsonModel
+    {
+            public int Id { get; set; }
+            public string Type { get; set; }
+            public string DefaultTranslation { get; set; }
+            public string CurrentTranslation { get; set; }
+            public string SuggestedTranslation { get; set; }
+    }
+}
