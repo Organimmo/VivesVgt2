9be275d8cd5dd17df7c8d092e39f5452fd262934
DeWintereArne <arne.dewintere@student.vives.be>
2022-05-09 12:19:27 +0200

    Implemented muli language

--- Organimmo/Organimmo.UI.Blazor/Program.cs
+++ Organimmo/Organimmo.UI.Blazor/Program.cs
@@ -1,11 +1,24 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Organimmo.UI.Blazor;
+using Organimmo.UI.Blazor.Extensions;
+namespace Organimmo.UI.Blazor
+{
+ public class Program
+ {
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.Services.AddLocalization();
+        builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-await builder.Build().RunAsync();
+        var host = builder.Build();
+        await host.SetDefaultCulture();     
+        await host.RunAsync();
+    }
+ }
+}
